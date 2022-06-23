6127e8f70992fcefb05bde397ff7569ea5b44e21
wcjameson <williamjameson90@gmail.com>
2022-06-22 17:03:01 -0700

    display photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react';
-import { isPlatform } from '@ionic/react'; 
+import { isPlatform } from '@ionic/react';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory, } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 import { Capacitor } from '@capacitor/core';
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
 export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([])
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
+    const fileName = new Date().getTime() + '.jpeg';
+    const newPhotos = [
+      {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      },
+      ...photos,
+    ];
+    setPhotos(newPhotos)
   };
 
   return {
+    photos,
     takePhoto,
   };
 }
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -19,7 +19,7 @@ import './Tab2.css';
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -33,6 +33,15 @@ const Tab2: React.FC = () => {
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
